74b4904da7f498c3fbebb0c540728f5b9de33dc6
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-08-22 08:47:53 +0900

    Fix mincut

--- passes/tests/mincut.cc
+++ passes/tests/mincut.cc
@@ -1,8 +1,12 @@
+#include "kernel/register.h"
+#include "kernel/celltypes.h"
+#include "kernel/log.h"
+#include "kernel/sigtools.h"
+
 #include "kernel/yosys.h"
 #include "kernel/satgen.h"
 #include "kernel/consteval.h"
 #include "kernel/celledges.h"
-#include "kernel/macc.h"
 
 #include <assert.h>
 #include <algorithm>
@@ -34,218 +38,250 @@ struct Mincut : public Pass {
 
   void execute(std::vector<std::string> args, RTLIL::Design* design) override
   {
-    log ("Mincut %lu!\n", args.size());
-    auto modules = design->modules();
+    log ("Mincut %s, %s!\n", args[0].c_str(), args[1].c_str());
+    // auto module = design->module(args[1]);
+    auto module = design->top_module();
+
 
     std::vector<hash_t> vec_grp_a;
     std::vector<hash_t> vec_grp_b;
 
+    log("Module Name = %s\n", module->name.c_str());
+    size_t init_num_cells = module->cells().size();
+    log("Cells = %zu\n", init_num_cells);
 
-    for (auto iter = modules.begin(); iter != modules.end(); iter++) {
-      log("Module Name = %s\n", (*iter)->name.c_str());
-      size_t init_num_cells = (*iter)->cells().size();
-      log("Cells = %zu\n", init_num_cells);
+    for (auto cell : module->cells()) {
+      m_vec_cell_hash.push_back (cell->hash());
+    }
 
-      for (auto cell : (*iter)->cells()) {
-        m_vec_cell_hash.push_back (cell->hash());
-      }
+    std::vector<std::vector<hash_t>> vec_cell_table(init_num_cells, std::vector<hash_t>(init_num_cells, 0));
 
-      std::vector<std::vector<hash_t>> vec_cell_table(init_num_cells, std::vector<hash_t>(init_num_cells, 0));
+    // Dump
+    for (auto cell : module->cells()) {
+      log("%x ", cell->hashidx_);
+    }
+    log ("\n");
 
-      // Dump
-      for (auto cell : (*iter)->cells()) {
-        log("%x ", cell->hashidx_);
-      }
-      log ("\n");
+    // Connections
+    for (auto cell : module->cells()) {
+      for (auto conn : cell->connections()) {
+        // log("%s =  %x\n", conn.first.c_str(),
+        //     get_hash (conn.second));
 
-      // Connections
-      for (auto cell : (*iter)->cells()) {
-        for (auto conn : cell->connections()) {
-          // log("%s =  %x\n", conn.first.c_str(),
-          //     get_hash (conn.second));
-
-          std::vector<hash_t> *target_cells = search_target_cell((*iter), cell->hash(), get_hash (conn.second));
-          for (auto target : *target_cells) {
-            hash_t cell_idx   = get_idx_by_hash(cell->hash());
-            hash_t target_idx = get_idx_by_hash(target);
-            // log ("cell_hash_idx(%08x)=%d, target_idx(%08x)=%d\n", cell->hash(), cell_idx, target, target_idx);
-            vec_cell_table[cell_idx][target_idx] = 1;
-          }
+        std::vector<hash_t> *target_cells = search_target_cell(module, cell->hash(), get_hash (conn.second));
+        for (auto target : *target_cells) {
+          hash_t cell_idx   = get_idx_by_hash(cell->hash());
+          hash_t target_idx = get_idx_by_hash(target);
+          // log ("cell_hash_idx(%08x)=%d, target_idx(%08x)=%d\n", cell->hash(), cell_idx, target, target_idx);
+          vec_cell_table[cell_idx][target_idx] = 1;
         }
       }
+    }
 
-      // Dump
-      // for (hash_t y = 0; y < vec_cell_table.size(); y++) {
-      //   log ("%10d : ", y);
-      //   for(hash_t x = 0; x < vec_cell_table.size(); x++) {
-      //     log ("%d ", vec_cell_table[y][x]);
-      //   }
-      //   log ("\n");
-      // }
+    // Dump
+    // for (hash_t y = 0; y < vec_cell_table.size(); y++) {
+    //   log ("%10d : ", y);
+    //   for(hash_t x = 0; x < vec_cell_table.size(); x++) {
+    //     log ("%d ", vec_cell_table[y][x]);
+    //   }
+    //   log ("\n");
+    // }
 
 
-      for (size_t i = 0; i < init_num_cells / 2; i++) {
-        vec_grp_a.push_back (m_vec_cell_hash[i]);
-      }
-      for (size_t i = init_num_cells / 2; i < init_num_cells; i++) {
-        vec_grp_b.push_back (m_vec_cell_hash[i]);
-      }
+    for (size_t i = 0; i < init_num_cells / 2; i++) {
+      vec_grp_a.push_back (m_vec_cell_hash[i]);
+    }
+    for (size_t i = init_num_cells / 2; i < init_num_cells; i++) {
+      vec_grp_b.push_back (m_vec_cell_hash[i]);
+    }
 
-      int tmp_cnt = 0;
-      do {
+    int tmp_cnt = 0;
+    do {
 
-        // log ("Mincut Do While Loop, tmp_cnt = %d\n", tmp_cnt);
+      // log ("Mincut Do While Loop, tmp_cnt = %d\n", tmp_cnt);
 
-        size_t num_cells = vec_grp_a.size() + vec_grp_b.size();
+      size_t num_cells = vec_grp_a.size() + vec_grp_b.size();
 
-        std::vector<int> gv;
-        std::vector<hash_t> av;
-        std::vector<hash_t> bv;
+      std::vector<int> gv;
+      std::vector<hash_t> av;
+      std::vector<hash_t> bv;
 
-        // Mincut iteration loop
-        for (size_t loop = 0; loop < num_cells / 2; loop++) {
+      // Mincut iteration loop
+      for (size_t loop = 0; loop < num_cells / 2; loop++) {
 
-          // log ("Mincut Iteration Loop : %zu, Num Cells = %zu, tmp_cnt = %d\n", loop, num_cells, tmp_cnt);
+        // log ("Mincut Iteration Loop : %zu, Num Cells = %zu, tmp_cnt = %d\n", loop, num_cells, tmp_cnt);
 
-          std::vector<int> cell_diff_table;
-          // log ("cell_diff_table : ");
-          for (auto a0 : vec_grp_a) {
-            int d = 0;
-            size_t a0_idx = get_idx_by_hash (a0);
-            for (auto a1 : vec_grp_a) {
-              size_t a1_idx = get_idx_by_hash (a1);
-              d += vec_cell_table[a0_idx][a1_idx];
-            }
-            for (auto b1 : vec_grp_b) {
-              size_t b1_idx = get_idx_by_hash (b1);
-              d -= vec_cell_table[a0_idx][b1_idx];
-            }
-            cell_diff_table.push_back(d);
-            // log ("%3d ", d);
+        std::vector<int> cell_diff_table;
+        // log ("cell_diff_table : ");
+        for (auto a0 : vec_grp_a) {
+          int d = 0;
+          size_t a0_idx = get_idx_by_hash (a0);
+          for (auto a1 : vec_grp_a) {
+            size_t a1_idx = get_idx_by_hash (a1);
+            d += vec_cell_table[a0_idx][a1_idx];
           }
-
-          // log (" | ");
-          for (auto b0 : vec_grp_b) {
-            int d = 0;
-            size_t b0_idx = get_idx_by_hash (b0);
-            for (auto b1 : vec_grp_b) {
-              size_t b1_idx = get_idx_by_hash (b1);
-              d += vec_cell_table[b0_idx][b1_idx];
-            }
-            for (auto a1 : vec_grp_a) {
-              size_t a1_idx = get_idx_by_hash (a1);
-              d -= vec_cell_table[b0_idx][a1_idx];
-            }
-            cell_diff_table.push_back(d);
-            // log ("%3d ", d);
+          for (auto b1 : vec_grp_b) {
+            size_t b1_idx = get_idx_by_hash (b1);
+            d -= vec_cell_table[a0_idx][b1_idx];
           }
-          // log ("\n");
-
-          // log ("g = DA + DB - 2AB tables :\n");
-          // find a from A and b from B, such that g = D[a] + D[b] − 2×c(a, b) is maximal
-          hash_t a_max_hash;
-          hash_t b_max_hash;
-          size_t a_max_idx;
-          size_t b_max_idx;
-          int    d_max = INT_MIN;
-          size_t a_grp_idx = 0;
-
-          // log ("     ");
-          // for (size_t i = 0; i < vec_grp_b.size(); i++) {
-          //   log ("%3zu ", i);
-          // }
-          // log ("\n");
-          for (auto a_hash: vec_grp_a) {
-            size_t a_idx = get_idx_by_hash (a_hash);
-            size_t b_grp_idx = 0;
-            // log ("%3zu : ", a_grp_idx);
-            for (auto b_hash: vec_grp_b) {
-              size_t b_idx = get_idx_by_hash (b_hash);
-              int g = cell_diff_table[a_grp_idx] + cell_diff_table[b_grp_idx] - 2 * vec_cell_table[a_idx][b_idx];
-              if (d_max < g) {
-                d_max = g;
-                a_max_hash = a_hash;
-                b_max_hash = b_hash;
-                a_max_idx = a_grp_idx;
-                b_max_idx = b_grp_idx;
-              }
-              // log ("%3d(%zu, %zu) ", g, a_idx, b_idx);
-              // log ("%3d ", g);
-              b_grp_idx ++;
-            }
-            // log ("\n");
-            a_grp_idx ++;
-          }
-
-          // log ("Result === \n");
-          // log ("  A_MAX_IDX = %u, B_MAX_IDX = %u, G = %d\n", a_max_hash, b_max_hash, d_max);
-
-          gv.push_back (d_max);
-          av.push_back (a_max_hash);
-          bv.push_back (b_max_hash);
-
-          // log ("idx: ");
-          // for (size_t i = 0; i < gv.size(); i++) {
-          //   log ("%3zu  ", i);
-          // }
-          // log ("\n");
-          // log ("gv : "); for (auto g: gv) { log ("%3d, ", g); } log ("\n");
-          // log ("av : "); for (auto a: av) { log ("%3d, ", a); } log ("\n");
-          // log ("bv : "); for (auto b: bv) { log ("%3d, ", b); } log ("\n");
-
-          vec_grp_a.erase(vec_grp_a.begin() + a_max_idx);
-          vec_grp_b.erase(vec_grp_b.begin() + b_max_idx);
-
+          cell_diff_table.push_back(d);
+          // log ("%3d ", d);
         }
 
-        // find k which maximizes g_max, the sum of gv[1], ..., gv[k]
-        int gv_total_max = INT_MIN;
-        size_t k_max = 0;
-        for (size_t k = 0; k < gv.size(); k++) {
-          int gv_total = 0;
-          for (size_t i = 0; i < k; i++) {
-            gv_total += gv[i];
+        // log (" | ");
+        for (auto b0 : vec_grp_b) {
+          int d = 0;
+          size_t b0_idx = get_idx_by_hash (b0);
+          for (auto b1 : vec_grp_b) {
+            size_t b1_idx = get_idx_by_hash (b1);
+            d += vec_cell_table[b0_idx][b1_idx];
           }
-          if (gv_total_max < gv_total) {
-            gv_total_max = gv_total;
-            k_max = k;
+          for (auto a1 : vec_grp_a) {
+            size_t a1_idx = get_idx_by_hash (a1);
+            d -= vec_cell_table[b0_idx][a1_idx];
           }
+          cell_diff_table.push_back(d);
+          // log ("%3d ", d);
         }
-        log ("tmp_cnt = %d, k_max = %zu, g_max = %d\n", tmp_cnt, k_max, gv_total_max);
+        // log ("\n");
 
-        if (gv_total_max <= 0) {
-          vec_grp_a.swap (av);
-          vec_grp_b.swap (bv);
+        // log ("g = DA + DB - 2AB tables :\n");
+        // find a from A and b from B, such that g = D[a] + D[b] − 2×c(a, b) is maximal
+        hash_t a_max_hash;
+        hash_t b_max_hash;
+        size_t a_max_idx;
+        size_t b_max_idx;
+        int    d_max = INT_MIN;
+        size_t a_grp_idx = 0;
+
+        // log ("     ");
+        // for (size_t i = 0; i < vec_grp_b.size(); i++) {
+        //   log ("%3zu ", i);
+        // }
+        // log ("\n");
+        for (auto a_hash: vec_grp_a) {
+          size_t a_idx = get_idx_by_hash (a_hash);
+          size_t b_grp_idx = 0;
+          // log ("%3zu : ", a_grp_idx);
+          for (auto b_hash: vec_grp_b) {
+            size_t b_idx = get_idx_by_hash (b_hash);
+            int g = cell_diff_table[a_grp_idx] + cell_diff_table[b_grp_idx] - 2 * vec_cell_table[a_idx][b_idx];
+            if (d_max < g) {
+              d_max = g;
+              a_max_hash = a_hash;
+              b_max_hash = b_hash;
+              a_max_idx = a_grp_idx;
+              b_max_idx = b_grp_idx;
+            }
+            // log ("%3d(%zu, %zu) ", g, a_idx, b_idx);
+            // log ("%3d ", g);
+            b_grp_idx ++;
+          }
+          // log ("\n");
+          a_grp_idx ++;
+        }
+
+        // log ("Result === \n");
+        // log ("  A_MAX_IDX = %u, B_MAX_IDX = %u, G = %d\n", a_max_hash, b_max_hash, d_max);
+
+        gv.push_back (d_max);
+        av.push_back (a_max_hash);
+        bv.push_back (b_max_hash);
+
+        // log ("idx: ");
+        // for (size_t i = 0; i < gv.size(); i++) {
+        //   log ("%3zu  ", i);
+        // }
+        // log ("\n");
+        // log ("gv : "); for (auto g: gv) { log ("%3d, ", g); } log ("\n");
+        // log ("av : "); for (auto a: av) { log ("%3d, ", a); } log ("\n");
+        // log ("bv : "); for (auto b: bv) { log ("%3d, ", b); } log ("\n");
+
+        vec_grp_a.erase(vec_grp_a.begin() + a_max_idx);
+        vec_grp_b.erase(vec_grp_b.begin() + b_max_idx);
+
+      }
+
+      // find k which maximizes g_max, the sum of gv[1], ..., gv[k]
+      int gv_total_max = INT_MIN;
+      size_t k_max = 0;
+      for (size_t k = 0; k < gv.size(); k++) {
+        int gv_total = 0;
+        for (size_t i = 0; i < k; i++) {
+          gv_total += gv[i];
+        }
+        if (gv_total_max < gv_total) {
+          gv_total_max = gv_total;
+          k_max = k;
+        }
+      }
+      log ("tmp_cnt = %d, k_max = %zu, g_max = %d\n", tmp_cnt, k_max, gv_total_max);
+
+      if (gv_total_max <= 0) {
+        vec_grp_a.swap (av);
+        vec_grp_b.swap (bv);
+        break;
+      }
+
+      vec_grp_a.clear();
+      vec_grp_b.clear();
+
+      for (size_t i = 0; i < k_max; i++) {
+        vec_grp_a.push_back (bv[i]);
+        vec_grp_b.push_back (av[i]);
+      }
+      for (size_t i = k_max; i < av.size(); i++) {
+        vec_grp_a.push_back (av[i]);
+      }
+      for (size_t i = k_max; i < bv.size(); i++) {
+        vec_grp_b.push_back (bv[i]);
+      }
+    } while (tmp_cnt++ < 100);
+
+    log ("Group A : \n");
+    Pass::call(design, "select -clear");
+    bool init = true;
+    for (auto a : vec_grp_a) {
+      for (auto cell : module->cells()) {
+        if (cell->hash() == a) {
+          std::string str = module->name.str() + "/" + cell->name.str();
+          log ("%s\n", str.c_str());
+          if (init) {
+            Pass::call(design, "select " + module->name.str() + "/" + cell->name.str());
+            init = false;
+          } else {
+            Pass::call(design, "select -add " + module->name.str() + "/" + cell->name.str());
+          }
           break;
         }
-
-        vec_grp_a.clear();
-        vec_grp_b.clear();
-
-        for (size_t i = 0; i < k_max; i++) {
-          vec_grp_a.push_back (bv[i]);
-          vec_grp_b.push_back (av[i]);
-        }
-        for (size_t i = k_max; i < av.size(); i++) {
-          vec_grp_a.push_back (av[i]);
-        }
-        for (size_t i = k_max; i < bv.size(); i++) {
-          vec_grp_b.push_back (bv[i]);
-        }
-      } while (tmp_cnt++ < 100);
+      }
     }
+    log ("-------------------------------\n");
+    // Pass::call(design, "select -list");
+    log ("-------------------------------\n");
+    Pass::call(design, "submod -name sub_0");
 
-    log ("Group A : ");
-    for (auto a : vec_grp_a) {
-      log ("%08x, ", a);
-    }
     log ("\n");
-    log ("Group B : ");
+
+    log ("Group B : \n");
+    init = true;
     for (auto b : vec_grp_b) {
-      log ("%08x, ", b);
+      for (auto cell : module->cells()) {
+        if (cell->hash() == b) {
+          std::string str = module->name.str() + "/" + cell->name.str();
+          log ("%s\n", str.c_str());
+          if (init) {
+            Pass::call(design, "select " + module->name.str() + "/" + cell->name.str());
+            init = false;
+          } else {
+            Pass::call(design, "select -add " + module->name.str() + "/" + cell->name.str());
+          }
+          break;
+        }
+      }
     }
+    Pass::call(design, "submod -name sub_1");
     log ("\n");
-
   }
 
   size_t get_idx_by_hash (hash_t hash) {
